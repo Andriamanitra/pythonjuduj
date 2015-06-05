e3aebc9e5327fb0c0cac0e50694efc8acd6801c6
Andriamanitra <mikko77792@gmail.com>
2015-06-05 17:03:11 +0300

    bättrened kanske

--- lastfm.py
+++ lastfm.py
@@ -3,7 +3,7 @@ from xml.etree import ElementTree as ET
 import datetime
 
 
-def np(username):
+def np(username, timeformat="%d.%m.%Y @ %H:%M"):
     r = requests.get("http://ws.audioscrobbler.com/1.0/user/%s/recenttracks.rss" % username)
     if r.status_code == requests.codes.ok:
         try:
@@ -12,9 +12,9 @@ def np(username):
             play_time = datetime.datetime.strptime(lastfm[0][9][2].text, "%a, %d %b %Y %H:%M:%S +0000")
             tz = datetime.datetime.now() - datetime.datetime.utcnow()
             play_time = play_time + tz
-            aika = play_time.strftime("%d.%m.%Y @ %H:%M")
+            aika = play_time.strftime(timeformat)
             link = lastfm[0][9][3].text
-            if play_time > ( datetime.datetime.now() - datetime.timedelta(minutes=8) ):
+            if play_time > datetime.datetime.utcnow() - datetime.timedelta(minutes=8):
                 return "Last.fm: {0} is now playing {1} | Link: {2}".format(username, now_playing, link)
             else:
                 return "Last.fm: {0} last listened to: {1} ({2}) | Link: {3}".format(username, now_playing, aika, link)
@@ -22,4 +22,3 @@ def np(username):
             return "No tracks found for user " + username
     else:
         return "Username not found"
-
